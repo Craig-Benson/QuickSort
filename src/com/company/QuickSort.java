283ce7face62476b0f7fcdf462290d710d725ff6
Craig-Benson <craigbenson@outlook.ie>
2020-02-09 13:47:48 +0000

    initial

---
+++ src/com/company/QuickSort.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Random;
+
+public class QuickSort {
+
+    int size = 10;
+    int[] intArray = new int[10];
+    Random random = new Random();
+
+
+    public void buildArray() {
+
+    for(int i = 0; i<intArray.length;i++){
+
+        intArray[i] = random.nextInt(10)+1;
+        System.out.println(intArray[i]);
+    }
+}
+    public void swap(){
+
+
+        int j= intArray[9];
+        int low= intArray[0];
+        int aux;
+
+        for(int i =0; intArray[i] < low;i++){
+            
+
+        }
+
+
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        QuickSort q = new QuickSort();
+        q.buildArray();
+        q.swap();
+
+
+
+
+    }
+}
+
+
