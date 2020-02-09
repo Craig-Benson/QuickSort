f67f5f4d51b850144ce43e47c802e4ea292e29ad
Craig-Benson <craigbenson@outlook.ie>
2020-02-09 16:21:06 +0000

    Merge branch 'master' of https://github.com/Craig-Benson/QuickSort
    # Conflicts:
    #	src/com/company/QuickSort.java
    
    test

--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -5,70 +5,85 @@ import java.util.Random;
 
 public class QuickSort {
 
+//    int size = 10;
+//    int[] intArray = new int[size];
+//    Random random = new Random();
+//    int low = 0;
+//    int aux;
+//
+//    public void buildArray() {
+//
+//    for(int i = 0; i<intArray.length;i++){
+//
+//    intArray[i] = random.nextInt(10)+1;
+//
+//
+//    }
+//public void buildArray() {
+//
+//    for(int i = 0; i<intArray.length;i++){
+//
+//        intArray[i] = random.nextInt(10)+1;
+//
+//System.out.println(Arrays.toString(intArray));
+//    }
+//}
+
     int size = 10;
-    int[] intArray = {10, 3, 5, 9, 8, 4, 1, 3, 7, 2};
+    int[] intArray = {7, 2, 2, 4, 1, 10, 1, 1, 8, 9};
     Random random = new Random();
     int low = 0;
     int aux;
 
-    public void buildArray() {
-
-//    for(int i = 0; i<intArray.length;i++){
-//
-////        intArray[i] = random.nextInt(10)+1;
-//        intArray[i] = random.nextInt(10)+1;
-//
-//    }
 
 
-        System.out.println(Arrays.toString(intArray));
-}
+
+
     public void swap() {
-
+        System.out.println(Arrays.toString(intArray));
         int i = 1;
         int j = 9;
 
-        while (i < j){
+        while (i < j) {
 
             while (intArray[low] > intArray[i]) {
-                    i++;
-                    if (i == intArray.length-1){
-                        aux = intArray[i];
-                        intArray[i] = intArray[low];
-                        intArray[low] = aux;
-                        break;
-                    }
+                i++;
+                if (i == intArray.length - 1) {
+                    aux = intArray[i];
+                    intArray[i] = intArray[low];
+                    intArray[low] = aux;
+                    break;
+                }
 
             }
-            if(i == j){break;}
+            if (i == j) {
+                break;
+            }
 
 
-
-            while(intArray[low] < intArray[j]) {
+            while (intArray[low] < intArray[j]) {
 
                 j--;
             }
 
+            if (intArray[i] == intArray[j]) {
+                i++;
+                j--;
+            }
             aux = intArray[i];
             intArray[i] = intArray[j];
             intArray[j] = aux;
             System.out.println(Arrays.toString(intArray));
-            i++;
-            j--;
+
         }
 
         System.out.println(Arrays.toString(intArray));
     }
 
 
-
-//    public boolean isSorted(){
-//      if(intArray[low] > intArray[] ==)
-//    }
-
     public static void main(String[] args) {
         QuickSort q = new QuickSort();
-        q.buildArray();
+//        q.buildArray();
         q.swap();
 
 
